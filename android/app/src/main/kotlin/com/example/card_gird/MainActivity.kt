9cecf18ce567cb41493a6ca08d4b3730221abd9b
sakhawat-cyber <sayed.infaceboob.com@gmail.com>
2021-12-08 03:54:51 +0600

    first

---
+++ android/app/src/main/kotlin/com/example/card_gird/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_gird
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/card_grad.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class card_Gride extends StatelessWidget {
+  const card_Gride({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: /*Column(
+        children: <Widget>[
+          Card(
+            elevation: 20,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),*/
+            Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        crossAxisCount: 4,
+        children: [
+          Container(
+            height: 200,
+            width: 150,
+            color: Colors.grey,
+          ),
+          Container(
+            height: 200,
+            width: 150,
+            color: Colors.red,
+          ),
+          Container(
+            height: 200,
+            width: 150,
+            child: Column(
+              children: [
+                Text("sayed"),
+              ],
+            ),
+            color: Colors.yellow,
+          ),
+          Container(
+            height: 200,
+            width: 150,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    )
+        //],
+        //),
+        );
+  }
+}
